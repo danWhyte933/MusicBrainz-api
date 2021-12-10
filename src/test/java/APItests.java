c5fa216de8b5b73c23a73c30e87c0350b0ebc925
Dan Whyte <danwhyte1986@gmail.com>
2021-12-10 15:47:00 +0000

    Add the API results to an ArrayList and attempt to call the lyrics API

--- src/test/java/APItests.java
+++ src/test/java/APItests.java
@@ -15,13 +15,15 @@ import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
 
 public class APItests {
 
     @Test
     void test1() {
 
-        Response responder = RestAssured.get("http://musicbrainz.org/ws/2/release/?query=radiohead&limit=100&fmt&inc=work=xml");
+        Response responder = RestAssured.get("http://musicbrainz.org/ws/2/release/?query=Metallica&limit=100&fmt&inc=work=xml");
 
 
         System.out.println("Status code : " + responder.getStatusCode());
@@ -31,8 +33,7 @@ public class APItests {
 
         String xmlResponse = responder.asString();
         Document doc = convertStringToXMLDocument(xmlResponse);
-
-
+        assert doc != null;
         doc.getDocumentElement().normalize();
 
         System.out.println("Root element: " + doc.getDocumentElement().getNodeName());
@@ -40,16 +41,31 @@ public class APItests {
         int theLength = nList.getLength();
         System.out.println("The length is " + theLength);
 
+
+        ArrayList<String> songs = new ArrayList();
         for (int i = 0; i < nList.getLength(); i++) {
             Node nNode = nList.item(i);
-            System.out.println("\nCurrent Element: " + nNode.getNodeName());
-            //this prints name for every time it is a tag
             if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-            System.out.println("Current Value: " + nNode.getTextContent());
+                String song = nNode.getTextContent();
+                songs.add(song);
             }
         }
-    }
 
+        System.out.println("Arraylist " + songs);
+        songs = new ArrayList<String>(new LinkedHashSet<String>(songs));
+        System.out.println("Without duplicates " + songs);
+
+
+
+        Response responderTwo = RestAssured.get("https://private-anon-1f438acdaa-lyricsovh.apiary-proxy.com/v1/Nirvana/Smells%20like%20teen%20spirit");
+        String lyricResponse = responderTwo.asString();
+        System.out.println("Lyric Status code : " + responderTwo.getStatusCode());
+        System.out.println("Lyric Body : " + responderTwo.getBody());
+        System.out.println("Lyric Time taken : " + responderTwo.getTime());
+        System.out.println("Lyric Header : " + responderTwo.getHeader("content-type"));
+        System.out.println(lyricResponse); // no lyrics found, gets 404 error
+
+    }
     private String getDocumentElementText(Document doc, String elementName) {
 
         return doc.getElementsByTagName(elementName).item(0).getTextContent();
